b2d65c43c44972977b956096fd93c3f1c2d88a65
jothamardel <ardelmbiplang@gmail.com>
2020-10-24 17:48:30 +0100

    Infinity scroll

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
 
 // Helper function to set attributes on DOM Elements.
@@ -11,9 +14,28 @@ function setAttributes(element, attributes) {
   }
 }
 
+
+// Unsplash API
+const count = 2;
+const apiKey = 'yaxjDHYjv14s3vQ74yA7i2ngxTddxspq-7HTkb9s1_E';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+// Check if all images were loaded.
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+
 // Create elements for Links and Photos, add to the DOM.
 
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // Run function for each object in Array.
   photosArray.forEach((photo) => {
     // Create an anchor element to link to unsplash.
@@ -31,17 +53,15 @@ function displayPhotos() {
       title: photo.alt_description
     });
 
+    // Event Listner, check when each is finished loading...
+
+    img.addEventListener('load', imageLoaded);
+
     // Put <img> inside <a> then put both inside our image container element.
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
 }
-
-
-// Unsplash API
-const count = 10;
-const apiKey = 'xxxxxxx';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Get photos from Unsplash API
 async function getPhotos() {
@@ -54,6 +74,14 @@ async function getPhotos() {
     console.log(error);
   }
 }
+
+// Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load
 
